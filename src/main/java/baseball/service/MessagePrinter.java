056b520bf38ec0aff5b05bd11560c997badb05a6
Ttungga <torb2002@gmail.com>
2022-04-18 11:22:45 +0900

    feat: add MessagePrinter

---
+++ src/main/java/baseball/service/MessagePrinter.java
@@ -0,0 +1,39 @@
+package baseball.service;
+
+import baseball.domain.Result;
+
+public class MessagePrinter {
+
+    private final String INPUT_REQUEST = "숫자를 입력해주세요: ";
+    private final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\r\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printInputRequestMessage() {
+        System.out.println(INPUT_REQUEST);
+    }
+
+    public void printResultMessage(final Result result) {
+        if (result.getBallCount() == 0 && result.getStrikeCount() == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        System.out.printf("%s %s\r\n", getBallMessage(result), getStrikeMessage(result));
+        if (result.getStrikeCount() == 3) {
+            System.out.println(GAME_OVER);
+        }
+    }
+
+    private String getBallMessage(final Result result) {
+        if (result.getBallCount() == 0) {
+            return "";
+        }
+        return result.getBallCount() + "볼";
+    }
+
+    private String getStrikeMessage(final Result result) {
+        if (result.getStrikeCount() == 0) {
+            return "";
+        }
+        return result.getStrikeCount() + "스트라이크";
+    }
+
+}

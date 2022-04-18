f1ab7584183fb2b278a416108a565ad8df9a0383
Ttungga <torb2002@gmail.com>
2022-04-18 20:22:20 +0900

    refactor: improve AnswerFactory.create logic

--- src/main/java/baseball/domain/AnswerFactory.java
+++ src/main/java/baseball/domain/AnswerFactory.java
@@ -1,17 +1,33 @@
 package baseball.domain;
 
-import baseball.utils.ThreeDigitNumber;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class AnswerFactory {
 
     public Answer create() {
-        int[] candidateDigits = null;
-        do {
-            candidateDigits = ThreeDigitNumber.toArray(Randoms.pickNumberInRange(100, 999));
-        } while (ThreeDigitNumber.hasDuplicateDigits(candidateDigits));
-
-        return Answer.of(candidateDigits[0], candidateDigits[1], candidateDigits[2]);
+        final int firstDigit = getFirstDigit();
+        final int secondDigit = getSecondDigit(firstDigit);
+        final int thirdDigit = getThirdDigit(firstDigit, secondDigit);
+        return Answer.of(firstDigit, secondDigit, thirdDigit);
     }
 
+    private int getFirstDigit() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+    private int getSecondDigit(final int firstDigit) {
+        int secondDigit = Randoms.pickNumberInRange(0, 9);
+        if (firstDigit == secondDigit) {
+            return getSecondDigit(firstDigit);
+        }
+        return secondDigit;
+    }
+
+    private int getThirdDigit(final int firstDigit, final int secondDigit) {
+        int thirdDigit = Randoms.pickNumberInRange(0, 9);
+        if (firstDigit == thirdDigit || secondDigit == thirdDigit) {
+            return getThirdDigit(firstDigit, secondDigit);
+        }
+        return thirdDigit;
+    }
 }
